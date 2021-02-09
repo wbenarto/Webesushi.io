7c1352a01c20464b06f5d4f4815ba0cd182b53af
w.benarto <w.benarto@gmail.com>
2021-02-09 14:23:11 -0800

    scroll trigger

--- landing/src/components/Intro.js
+++ landing/src/components/Intro.js
@@ -1,19 +1,90 @@
-import React, { useRef, useEffect } from "react";
-import { TweenMax, Power3, ScrollTrigger, gsap } from "gsap";
+import React, { useRef, useEffect, useState, useLayoutEffect } from "react";
+import { TweenMax, Power3, gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
+const sections = [
+  { title: "Title 1", subtitle: "$15" },
+  {
+    title: "Salmon Tataki",
+    subtitle: "$34",
+  },
+  {
+    title: "Tuna Nigiri",
+    subtitle: "$25",
+  },
+];
 const Intro = () => {
+  const [bg, setBg] = useState("#262626");
+
+  const toggleBackground = () => {
+    const color = bg !== "#262626" ? "#5a7d95" : "#1b4943";
+    setBg(color);
+  };
+
   const headerRef = useRef(null);
   let logoItem = useRef(null);
   let h1 = useRef(null);
+  let blue = useRef(null);
+
+  const revealRefs = useRef([]);
+  revealRefs.current = [];
+  const addToRefs = (e) => {
+    if (e && !revealRefs.current.includes(e)) {
+      revealRefs.current.push(e);
+    }
+    console.log(revealRefs.current);
+  };
+
+  useEffect(() => {
+    gsap.to(headerRef.current, {
+      duration: 2,
+      backgroundColor: bg,
+      x: "-300px",
+      ease: "easeOut",
+      height: "100%",
+    });
+  }, [bg]);
+
   useEffect(() => {
     console.log(logoItem);
 
-    gsap.from(headerRef.current, {
-      x: "-200px",
-      duration: 1,
+    gsap.from(h1.current, {
+      x: "-600px",
+      duration: 2,
       autoAlpha: 0,
       ease: "easeIn",
+      height: "100%",
       delay: 1,
+    });
+
+    gsap.from(blue.current, {
+      x: "600px",
+      duration: 2,
+      autoAlpha: 0,
+      ease: "easeOut",
+      delay: 1,
+    });
+
+    revealRefs.current.forEach((e, i) => {
+      gsap.fromTo(
+        e,
+        {
+          autoAlpha: 0,
+        },
+        {
+          duration: 1,
+          autoAlpha: 1,
+          ease: "none",
+          scrollTrigger: {
+            id: `App-section-${i + 1}`,
+            trigger: e,
+            start: "top center+=100",
+            toggleActions: "play none none reverse",
+            markers: true,
+          },
+        }
+      );
     });
   }, []);
   console.log(logoItem);
@@ -22,8 +93,9 @@ const Intro = () => {
     <div className="intro">
       <div ref={headerRef} className="hero">
         <h1 ref={h1}>The New Golden Salmon Tataki</h1>
+        <button onClick={toggleBackground}>Toggle Bg-color</button>
       </div>
-      <div className="trigger"></div>
+      <div ref={blue} className="trigger"></div>
       <iframe
         className="logoItem"
         ref={logoItem}
@@ -34,7 +106,14 @@ const Intro = () => {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
       ></iframe>
-
+      {sections.map(({ title, subtitle }) => {
+        return (
+          <div key={title} className="App-section" ref={addToRefs}>
+            <h2>{title}</h2>
+            <p>{subtitle}</p>
+          </div>
+        );
+      })}
       {/* <video alt="Test Video" src="/test.mp4" type="video/mp4"></video> */}
     </div>
   );
