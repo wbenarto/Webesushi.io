66e3d09c2d54bc9ad2b141d4a5dac441c4eadb64
w.benarto <w.benarto@gmail.com>
2021-03-22 20:56:44 -0700

    home

--- landing/src/components/Recipes/HomemakaseElements.js
+++ landing/src/components/Recipes/HomemakaseElements.js
@@ -19,7 +19,7 @@ export const HomemakaseContainer = styled.div`
 
   ::before {
     content: "";
-    background-color: rgba(0, 0, 0, 0.4);
+    background-color: rgba(0, 0, 0, 0.6);
     background-size: cover;
     position: absolute;
     top: 0px;
@@ -76,12 +76,17 @@ export const AppMain = styled.div`
   margin-right: auto;
   margin-bottom: auto;
   max-width: 70vw;
-  height: calc(100vh - 17vh);
+  height: calc(100vh - 24vh);
   background-color: transparent;
   /* #a5b4cc; */
+  overflow: scroll;
+  color: white;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 
   @media screen and (max-width: 475px) {
-    min-width: 100%;
+    min-width: 90%;
   }
 `;
 
@@ -96,7 +101,7 @@ export const AppFooter = styled.div`
   justify-content: space-evenly;
 
   @media screen and (max-width: 475px) {
-    height: 10vh;
+    height: 16vh;
   }
 `;
 
@@ -123,4 +128,72 @@ export const AppIconsTitle = styled.div`
   font-size: 0.6rem;
   color: white;
   text-align: center;
+`;
+
+export const SushiCategory = styled.div`
+  margin-top: 25px;
+  width: 100%;
+  height: 100px;
+
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
+
+export const SushiCategoryButton = styled.div`
+  flex-basis: 25%;
+  height: 2rem;
+
+  font-size: 1.2rem;
+  background-color: transparent;
+  color: black;
+  border-radius: 10px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  :nth-child(1) {
+    background-color: #db405f;
+  }
+  :nth-child(2) {
+    background-color: #eded4a;
+  }
+  :nth-child(3) {
+    background-color: #8df27e;
+  }
+`;
+
+export const SushiCategoryDesc = styled.div`
+  width: 100%;
+
+  position: relative;
+  top: 0;
+  color: ${({ cat }) =>
+    cat == "raw"
+      ? "white"
+      : cat == "cooked"
+      ? "black"
+      : cat == "vegetarian"
+      ? "black"
+      : "white"};
+
+  background-color: ${({ cat }) =>
+    cat == "raw"
+      ? "#db405f"
+      : cat == "cooked"
+      ? "#eded4a"
+      : cat == "vegetarian"
+      ? "#8df27e"
+      : "#db405f"};
+  border-radius: 5px;
+
+  > p {
+    margin-bottom: 10px;
+    padding: 10px;
+  }
+
+  @media screen and (max-width: 475px) {
+    margin-bottom: 0;
+  }
 `;
--- landing/src/pages/homemakase.js
+++ landing/src/pages/homemakase.js
@@ -23,6 +23,9 @@ import {
   AppIconsWrapper,
   AppIcons,
   AppIconsTitle,
+  SushiCategory,
+  SushiCategoryButton,
+  SushiCategoryDesc,
 } from "../components/Recipes/HomemakaseElements";
 
 import {
@@ -34,6 +37,7 @@ import {
 const HomeMakase = () => {
   const [active, setActive] = useState("/");
   const [isOpen, setIsOpen] = useState(false);
+  const [category, setCategory] = useState("raw");
 
   const toggle = () => {
     setIsOpen(!isOpen);
@@ -44,6 +48,10 @@ const HomeMakase = () => {
     console.log(page);
   };
 
+  const handleCategory = (category) => {
+    console.log(category);
+    setCategory(category);
+  };
   return (
     <div>
       <HomemakaseContainer>
@@ -61,27 +69,92 @@ const HomeMakase = () => {
             <Shop />
           ) : (
             <div>
-              <h1>Homemakase</h1>
-              <p>
-                We will first take a look what kind of ingredients and tools are
-                needed before we start our prepping process.
-              </p>
-
               <h2>Omakase</h2>
-              <p>We eat good usually here in omakase</p>
-
-              <h2>To Get Started</h2>
               <p>
-                Let's get all the ingredients and tools needed to start making
-                sushi at home. Go to "At Market" tab to see more.
+                Omakase (Japanese: お任せ, Hepburn: o-makase) is a Japanese
+                phrase, used when ordering sushi in restaurants, that means
+                'I'll leave it up to you' (from Japanese 'to entrust' (任せる,
+                makaseru)).
               </p>
+              <br />
+              <h2>Homemakase</h2>
+              <p>
+                Here we will help you recreate your favorite sushi dish at the
+                comfort of your home. <br />
+                <br /> We will first take a look what kind of ingredients and
+                tools needed before we start our prepping process.
+              </p>
+              <br />
+
               <h2>Let's get rollin!~</h2>
               <p>
-                Go to "Get Rollin" tab to access in depth step-by-step
-                instruction for prepping your ingredients. I also included
-                recipes for the sushi I have made in the past. Don't forget to
-                try out the sauces recipes as well!{" "}
+                Go to "At Market" tab to see all necessary tools and ingredients
+                to make your sushi. Then go to "Get Rollin" tab to access in
+                depth step-by-step instruction for prepping your ingredients. I
+                also included recipes for the sushi I have made in the past.
+                Don't forget to try out the sauces recipes as well!
               </p>
+              <br />
+
+              <p>Here are some categories to help you navigate the site:</p>
+
+              <SushiCategory>
+                <SushiCategoryButton
+                  cat={category}
+                  onClick={() => handleCategory("raw")}
+                >
+                  Raw
+                </SushiCategoryButton>
+                <SushiCategoryButton
+                  cat={category}
+                  onClick={() => handleCategory("cooked")}
+                >
+                  Cooked
+                </SushiCategoryButton>
+                <SushiCategoryButton
+                  cat={category}
+                  onClick={() => handleCategory("vegetarian")}
+                >
+                  Vegetarian
+                </SushiCategoryButton>
+                {category == "raw" ? (
+                  <SushiCategoryDesc cat={category}>
+                    <p>
+                      Everything shown in this category will be consisting of
+                      raw ingredients. Consuming raw or undercooked meats,
+                      poultry, seafood, shellfish, or eggs may increase your
+                      risk of foodborne illness. Please always keep raw fish
+                      under 41°F prior to serving and confirm with your fish
+                      supplier if the product is safe for raw consumption.
+                    </p>
+                  </SushiCategoryDesc>
+                ) : category == "cooked" ? (
+                  <SushiCategoryDesc cat={category}>
+                    <p>
+                      So many people got introduced into eating sushi from their
+                      first "Shrimp Tempura Roll". If you are not sure about
+                      dealing with raw fish, selections in this category will
+                      definitely give you some learning experience. If you are
+                      not sure about dealing with raw fish, selections in this
+                      category will definitely give you some learning
+                      experience.{" "}
+                    </p>
+                  </SushiCategoryDesc>
+                ) : category == "vegetarian" ? (
+                  <SushiCategoryDesc cat={category}>
+                    <p>
+                      This category is for my vegetarian friends! We will
+                      explore different ways of serving vegetarian sushi and
+                      have fun with it. If you are vegan, we will provide more
+                      recipes in the future. If you are not sure about dealing
+                      with raw fish, selections in this category will definitely
+                      give you some learning experience.{" "}
+                    </p>
+                  </SushiCategoryDesc>
+                ) : (
+                  <SushiCategoryDesc cat={category}>Raw</SushiCategoryDesc>
+                )}
+              </SushiCategory>
             </div>
           )}
 
